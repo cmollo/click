75f180e32ff3e65e597a55099aa8ef20d991138f
FaytxZen <andrewvora@ufl.edu>
2014-07-23 15:57:16 -0400

    Added .todelete to stylesheet. Added a simple click listener to clickEngine object. Linked engine to index.html.

--- scripts/clickengine.js
+++ scripts/clickengine.js
@@ -1,6 +1,33 @@
+//GLOBAL VARIABLES
+var canvasElement = $('#canvas');
+
 //CLICK ENGINE OBJECT
-var clickEngine = {};
+var clickEngine = {};//create empty object
+clickEngine.clickCount = 0;
 
-$document.ready(function(){
+//Click Listener function
+clickEngine.onClick = function(){
+	var canvasElement = $('#canvas');
+	var insert = "<p class='todelete'>Stop. "+clickEngine.clickCount+"</p>";
+	canvasElement.append(insert);
 
+	clickEngine.clickCount++;
+};
+
+clickEngine.init = function(){
+	var canvasElement = $('#canvas');
+
+	//attach click listener
+	canvasElement.on('click',clickEngine.onClick);
+	
+	//remove .todelete elements
+	setInterval(function(){
+		$('.todelete').fadeOut('fast');
+		clickEngine.clickCount = 0;
+	},3000);
+};
+
+
+$(document).ready(function(){
+	clickEngine.init();
 });
